3bd6c03f2e70252fe183897c72e5c69df4bb9847
Kazuyuki Jitsukata <kjitsukata@gmail.com>
2019-08-03 18:24:37 +0900

    [add] added template for debian 10

---
+++ debian-10-buster/script/setup-base.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+VAGRANT_USER='vagrant'
+VAGRANT_GROUP='vagrant'
+VAGRANT_HOME=/home/${VAGRANT_USER}
+VAGRANT_KEY_URL='https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub'
+
+# installing packages
+echo "APT update"
+sudo apt-get -y update && sudo apt-get -y upgrade
+echo "Installing Packages"
+sudo apt-get install -y build-essential curl wget git perl libssl-dev dkms linux-image-amd64 linux-headers-`uname -r`
+
+# installing vagrant keys
+echo "Installing Vagrant User Keys"
+mkdir -pm 700 ${VAGRANT_HOME}/.ssh
+wget --no-check-certificate ${VAGRANT_KEY_URL} -O ${VAGRANT_HOME}/.ssh/authorized_keys
+chmod 600 ${VAGRANT_HOME}/.ssh/authorized_keys
+chown -R ${VAGRANT_USER}:${VAGRANT_GROUP} ${VAGRANT_HOME}/.ssh
