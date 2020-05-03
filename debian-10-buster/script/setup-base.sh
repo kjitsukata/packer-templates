ddf4e01674da2c586889ca0ca8317b47e46d3d6d
Kazuyuki Jitsukata <kjitsukata@gmail.com>
2020-05-04 00:31:05 +0900

    [update] add sysstat to install package
    Enabled to start sysstat automatically.
    Set the acquisition interval to 1 minute and saved it for 1 month (31 days).

--- debian-10-buster/script/setup-base.sh
+++ debian-10-buster/script/setup-base.sh
@@ -9,7 +9,13 @@ VAGRANT_KEY_URL='https://raw.githubusercontent.com/mitchellh/vagrant/master/keys
 echo "APT update"
 sudo apt-get -y update && sudo apt-get -y upgrade
 echo "Installing Packages"
-sudo apt-get install -y build-essential curl wget git perl debconf-utils libssl-dev dkms linux-image-amd64 linux-headers-`uname -r`
+sudo apt-get install -y build-essential curl wget git perl debconf-utils libssl-dev dkms linux-image-amd64 linux-headers-`uname -r` sysstat
+
+# configure sysstat
+sudo sed -i -e 's/ENABLED="false"/ENABLED="true"/g' /etc/default/sysstat
+sudo sed -i -e "s/^5-55\/10/5-55\/1/g" /etc/cron.d/sysstat
+sudo sed -i -e "s/^HISTORY=7/HISTORY=31/g" /etc/sysstat/sysstat
+sudo systemctl enable sysstat.service
 
 # installing vagrant keys
 echo "Installing Vagrant User Keys"
@@ -17,3 +23,4 @@ mkdir -pm 700 ${VAGRANT_HOME}/.ssh
 wget --no-check-certificate ${VAGRANT_KEY_URL} -O ${VAGRANT_HOME}/.ssh/authorized_keys
 chmod 600 ${VAGRANT_HOME}/.ssh/authorized_keys
 chown -R ${VAGRANT_USER}:${VAGRANT_GROUP} ${VAGRANT_HOME}/.ssh
+
