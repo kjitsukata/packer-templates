00fb07f46afbc8cb24046e4bcb7fd4f1bdcfb045
Kazuyuki Jitsukata <kjitsukata@gmail.com>
2021-01-04 00:28:46 +0900

    [add] Add Oh-My-Zsh and Powerlevel10k for vagrant user

--- debian-10-buster/script/setup-base.sh
+++ debian-10-buster/script/setup-base.sh
@@ -5,7 +5,7 @@ VAGRANT_GROUP='vagrant'
 VAGRANT_HOME=/home/${VAGRANT_USER}
 VAGRANT_KEY_URL='https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub'
 
-# installing packages
+# install packages
 echo "APT update"
 sudo apt update -y && sudo apt upgrade -y
 echo "Installing Packages"
@@ -17,7 +17,7 @@ sudo sed -i -e "s/^5-55\/10/5-55\/1/g" /etc/cron.d/sysstat
 sudo sed -i -e "s/^HISTORY=7/HISTORY=31/g" /etc/sysstat/sysstat
 sudo systemctl enable sysstat.service
 
-# installing vagrant keys
+# install vagrant keys
 echo "Installing Vagrant User Keys"
 mkdir -pm 700 ${VAGRANT_HOME}/.ssh
 wget --no-check-certificate ${VAGRANT_KEY_URL} -O ${VAGRANT_HOME}/.ssh/authorized_keys
@@ -26,3 +26,7 @@ chown -R ${VAGRANT_USER}:${VAGRANT_GROUP} ${VAGRANT_HOME}/.ssh
 
 # change shell for vagrant user
 sudo /bin/chsh -s /bin/zsh vagrant
+
+# install Oh-My-Zsh and Powerlevel10k for vagrant user
+sudo -u vagrant sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo -u vagrant git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
