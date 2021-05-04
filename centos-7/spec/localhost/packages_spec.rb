4170d3d4477b4d79d5ffc57fb789658e3365e184
Kazuyuki Jitsukata <kjitsukata@gmail.com>
2021-05-04 16:15:13 +0900

    [remove] Deleted unnecessary files for serverspec

--- centos-7/spec/localhost/packages_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-%w{gcc make perl kernel-headers kernel-devel curl wget git epel-release dkms}.each do |pkg|
-  describe package(pkg), :if => os[:family] == 'redhat' do
-    it { should be_installed }
-  end
-end
